ec00494656661609b3973e167bad225895794cf2
zeraye <zeraye@protonmail.com>
2024-02-29 23:22:06 +0100

    feat: display `Analysis`, `Chestboard` and `Timer` next to each other

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { Chess } from "chess.js";
+
+import { useState } from "react";
+
 import Analysis from "./components/Analysis/Analysis";
 import Chessboard from "./components/Chessboard/Chessboard";
 import Timer from "./components/Timer/Timer";
 
 const App = () => {
+  const [chess, setChess] = useState(new Chess());
+
   return (
-    <>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns:
+          "minmax(20%, max-content) auto minmax(20%, max-content)",
+        alignItems: "center",
+      }}
+    >
       <Analysis />
-      <Chessboard />
+      <Chessboard chess={chess} />
       <Timer />
-    </>
+    </div>
   );
 };
 
