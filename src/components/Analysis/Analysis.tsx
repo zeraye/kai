0c5e4784461fdb32894aa442a0444842541a6235
zeraye <zeraye@protonmail.com>
2024-02-29 23:21:31 +0100

    feat: add basic styling to `Analysis` and `Timer` component

--- src/components/Analysis/Analysis.tsx
+++ src/components/Analysis/Analysis.tsx
@@ -1,5 +1,18 @@
 const Analysis = () => {
-  return <p>Analysis</p>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div>
+        <h2>Depth searched: 7</h2>
+        <h2>Evaluation: 0.15</h2>
+        <h2>Best move: Be3</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Analysis;
--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,5 +1,11 @@
 const Timer = () => {
-  return <p>Timer</p>;
+  return (
+    <div style={{ textAlign: "center", padding: "3em" }}>
+      <h1>2:23</h1>
+      <hr />
+      <h1>1:12</h1>
+    </div>
+  );
 };
 
 export default Timer;
