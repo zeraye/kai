aa907ae0f404a8e5a509c385515bf312b874c0bd
zeraye <zeraye@protonmail.com>
2024-03-13 14:42:57 +0100

    feat: add theme from `mui`

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
-import "./index.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
